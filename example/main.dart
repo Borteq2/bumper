f5c14f74116e9d3222c600931e09eaa4c3032825
borteq-deck <borteq@yandex.ru>
2024-12-06 15:57:25 +0200

    - add example

---
+++ example/main.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main() {
+  stdout.writeln('This is an example of how to use the `bumper` tool:');
+  stdout.writeln('To bump a version part, run the following command:');
+  stdout.writeln('dart run bumper major');
+  stdout.writeln('Or simply run `dart run bumper` to choose interactively.');
+}
